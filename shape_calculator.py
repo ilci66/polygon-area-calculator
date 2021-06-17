e1d2c7177d92272cace5eaeca2236f287f3cff4c
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-06-17 16:07:22 +0200

    standard upload

---
+++ shape_calculator.py
@@ -0,0 +1,13 @@
+class Rectangle:
+  def __init__(self, width, height):
+    self.witdh=width
+    self.height=height
+
+  def set_width(self):
+    return self.witdh
+
+  def set_height(self):
+    return self.height
+    
+class Square(Rectangle):
+  pass
